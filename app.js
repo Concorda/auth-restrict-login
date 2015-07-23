6d72f8faeaeca45a493fa83998dcb9d58a495978
mirceaalexandru <mircea.alexandru@gmail.com>
2015-07-23 19:48:39 +0300

    added code to allow or not login based on a cookie

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ module.exports = function ( options ) {
   function restrict_login(args, cb){
     var req = this.fixedargs.req$
 
-    cb(null, {ok: true})
+    var cookie_val = req.seneca.cookies.get( 'allow_login' )
+    if (1 == cookie_val){
+      cb(null, {ok: true})
+    }else{
+      cb(null, {ok: false, why: 'not-allowed'})
+    }
   }
 
   seneca.add({role: 'auth', restrict: 'login'}, restrict_login)
