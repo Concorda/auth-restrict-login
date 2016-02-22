d01706048dae85e3b83f7f1a0bf8ad6f11adc7f2
malex <mircea.alexandru@gmail.com>
2016-02-22 12:52:47 +0200

    remove unnecessary code

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ module.exports = function (options) {
   var seneca = this
 
   function restrict_login (args, cb) {
-    var req = this.fixedargs.req$
-
-    var cookie_val = req.seneca.cookies.get('allow_login')
-    if (1 === cookie_val) {
-      cb(null, {ok: true})
-    }
-    else {
-      cb(null, {ok: false, why: 'not-allowed'})
-    }
+    cb(null, {ok: true})
   }
 
   seneca.add({role: 'auth', restrict: 'login'}, restrict_login)
